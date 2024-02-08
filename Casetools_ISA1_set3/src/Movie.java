0c6da8970dd10523ede55e4350d77b32bd1631be
exam <exam@Bcalab4-m01>
2024-02-08 09:12:18 +0530

    dfgdrgr

---
+++ Casetools_ISA1_set3/src/Movie.java
@@ -0,0 +1,13 @@
+
+public class Movie {
+
+	private String title;
+	private String director;
+	public Movie(String title, String director) {
+		this.title = title;
+		this.director = director;
+	}
+	public String getTitle() {
+		return this.title;
+	}
+}
